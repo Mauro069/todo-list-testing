90a93fd3b922c8d241c889860a9a4922ea48560e
Mauro Vera <Maurovera069@gmail.com>
2023-09-27 11:32:30 -0300

    Organice imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,16 @@
-import { NgModule } from '@angular/core';
+import { TodoListItemComponent } from './components/todo-list-item/todo-list-item.component';
+import { TodoFiltersComponent } from './components/todo-filters/todo-filters.component';
+import { TodoOrdersComponent } from './components/todo-orders/todo-orders.component';
+import { ArrowComponent } from './components/dropdown/components/arrow.component';
+import { TodoListComponent } from './components/todo-list/todo-list.component';
+import { TodoFormComponent } from './components/todo-form/todo-form.component';
+import { DropdownComponent } from './components/dropdown/dropdown.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TodoListComponent } from './components/todo-list/todo-list.component';
-import { TodoFormComponent } from './components/todo-form/todo-form.component';
-import { TodoListItemComponent } from './components/todo-list-item/todo-list-item.component';
 import { FormsModule } from '@angular/forms';
-import { TodoFiltersComponent } from './components/todo-filters/todo-filters.component';
-import { TodoOrdersComponent } from './components/todo-orders/todo-orders.component';
 import { DatePipe } from './pipes/date.pipe';
-
-import { DropdownComponent } from './components/dropdown/dropdown.component';
-import { ArrowComponent } from './components/dropdown/components/arrow.component';
+import { NgModule } from '@angular/core';
 
 @NgModule({
   declarations: [
